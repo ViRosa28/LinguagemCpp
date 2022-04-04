de69706513e6cbcbc663a2b57b9856fdd79b4941
ViRosa <vitoriadanielrosa@gmail.com>
2022-04-04 08:52:28 -0300

    Estudo C++

---
+++ 1-01042022/primeiro.cpp
@@ -0,0 +1,12 @@
+//Programa para exibir uma mensagem. O código foi escrito em c++
+//Importação da biblioteca de entrada e saída de dados
+
+#include <iostream>
+
+int main(){
+
+    //Saída padrão de dados
+    std::cout <<"Bem Vindo ao C++\n";
+
+    return 0;
+}
